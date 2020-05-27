f51d7d559b2d86c5280a73ae8e78fc7f3c9246ee
basbeu <bastien.beuchat@epfl.ch>
2020-05-28 01:40:38 +0200

    Improve leaderboard

--- web/website/js/race.js
+++ web/website/js/race.js
@@ -23,25 +23,24 @@ function setup(date, file, isMen) {
     }
 
 
-    var header_name = ["#", "", "Name", "Country", "Time"];
-
+    var header_name = ["#", "", "Name", "", "Time"];
+    function getPhoto(photo){
+        return photo===''?'./img/placeholder_male.jpg':photo
+    }
     // use es6 string templates to populate rows
     const rowTemplate = (d) => {
         return `
   <td>${d.ath_rank}</td>
-  <td> <img class="img-fluid rounded-circle" src=${'"' + d.photo + '"'}</td>
+  <td> <img class="img-fluid rounded-circle race-ath-photo" src=${'"' + getPhoto(d.photo) + '"'}</td>
   <td>${d.ath_name}</td>
-  <td>${d.ath_country_name}</td>
+  <td><img class="img-fluid race-country-flag" src="https://www.countryflags.io/${d.ath_country}/flat/64.png" alt="${d.ath_country_name}" ></td>
   <td>${centisToMinutesAndSeconds(d.ath_time)}</td>
 
   `;
     };
 
-
     // read data from the url
     d3.csv(file).then(function (data) {
-
-
         var filtered = data.filter(d => d.date === date);
         // select viz and append table
 
@@ -50,7 +49,7 @@ function setup(date, file, isMen) {
         d3.select("#country_name").text(date);
         var src = "./img/events/" + venueImage(filtered[0].venue);
         console.log(encodeURI(src));
-        $("#race-image").html('<img class="img-fluid rounded" src='+encodeURI(src)+'>');
+        $("#race-image").html('<img class="img-fluid rounded race-event-photo" src='+encodeURI(src)+'>');
 
         var src = "https://www.countryflags.io/" + filtered[0].country + "/flat/64.png";
         $('#flag').html('<img src="'+src+'" class="img-fluid rounded">')
